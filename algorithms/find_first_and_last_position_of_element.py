084c289cdcad407b2d4c185134457c6e36d003a8
Sovan <sovan.panda338@gmail.com>
2022-09-04 08:49:41 +0530

    find first and last position of element in list

--- algorithms/find_first_and_last_position_of_element.py
+++ algorithms/find_first_and_last_position_of_element.py
@@ -1,6 +1,47 @@
 #################################################################################
 ## Youtube Link: https://www.youtube.com/watch?v=bU-q1OJ0KWw&ab_channel=NickWhite
 ## Find the first and last position of element in a sorted array with time complexity O(log N)
-## Example: input: items = [5, 7, 7, 8, 8, 10] target = 8 Output: [3,4]
-## Example: input: items = [5, 7, 7, 8, 8, 10] target = 3 Output: [-1, -1]
+## Example: input: list_items = [5, 7, 7, 8, 8, 10] target_item = 8 Output: [3,4]
+## Example: input: list_items = [5, 7, 7, 8, 8, 10] target_item = 3 Output: [-1, -1]
 ##################################################################################
+
+#Solution with time complexity O(log N)
+def find_start_index(list_items, target_item):
+    start_point = 0
+    end_point = len(list_items)
+    index_value = -1
+    while start_point <= end_point:
+        midpoint = (end_point + start_point)//2
+        #get the minimum index value of the target_item item
+        if target_item <= list_items[midpoint]:
+            end_point = midpoint - 1
+        else:
+            start_point = midpoint + 1
+        if target_item == list_items[midpoint]:
+            index_value = midpoint
+    return index_value
+
+
+def find_end_index(list_items, target_item):
+    start_point = 0
+    end_point = len(list_items)
+    index_value = -1
+    while start_point <= end_point:
+        midpoint = (end_point + start_point) // 2
+        # get the maximum index value of the target_item item
+        if target_item >= list_items[midpoint]:
+            start_point = midpoint + 1
+        else:
+            end_point = midpoint - 1
+        if target_item == list_items[midpoint]:
+            index_value = midpoint
+    return index_value
+
+
+if __name__ == "__main__":
+    list_items = [5, 7, 7, 8, 8, 10]
+    target_item = 8
+    start_index = find_start_index(list_items, target_item)
+    end_index = find_end_index(list_items, target_item)
+    result = [start_index, end_index]
+    print(f'First and last position of element {target_item} and position are {result}')
