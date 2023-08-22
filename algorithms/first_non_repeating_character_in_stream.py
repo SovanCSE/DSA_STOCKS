c7ea444352575c2199769bb959a90aaaa067d85b
Sovan Panda <35234742+SovanCSE@users.noreply.github.com>
2023-08-22 18:57:12 +0530

    Update first_non_repeating_character_in_stream.py

--- algorithms/first_non_repeating_character_in_stream.py
+++ algorithms/first_non_repeating_character_in_stream.py
@@ -20,26 +20,35 @@
 ## Input string: AAAC ## Output: A-1-1C
 ############################################################################################
 
-hash_array = [0 for _ in range(26) ]
-queue_list = []
-str_values = "AAAC".lower() 
+def find_first_non_repeating_character_in_stream(str_values):
+    hash_array = [0 for _ in range(26)]
+    queue_list = []
+    
+    if not all(c.isalpha() for c in str_values):
+        print('String contains other letter than alphabet')
+        return None
+    
+    #traverse the whole stream
+    for char in str_values:
 
-#traverse the whole stream
-for char in str_values:
-    
-    ## Push each character in queue
-    queue_list.append(char)
-    
-    ## Increment frequency in hash array
-    hash_array[ord(char)-97] += 1
-    
-    ## Check for non repeating character
-    while queue_list:
-        ## If frequency of front item of the queue is more than 1 then we pop that element
-        if hash_array[ord(queue_list[0])-97] > 1:
-            queue_list.pop(0)
+        ## Push each character in queue
+        queue_list.append(char)
+
+        ## Increment frequency in hash array
+        if char.isupper():
+            starting_ascii_value = 65
         else:
-            print(queue_list[0], end=' ')
-            break
-    if not queue_list:
-        print(-1, end=' ')
+            starting_ascii_value = 97
+        hash_array[ord(char)-starting_ascii_value] += 1
+
+        ## Check for non repeating character
+        while queue_list:
+            ## If frequency of front item of the queue is more than 1 then we pop that element
+            if hash_array[ord(queue_list[0])-starting_ascii_value] > 1:
+                queue_list.pop(0)
+            else:
+                print(queue_list[0], end=' ')
+                break
+        if not queue_list:
+            print(-1, end=' ')
+find_first_non_repeating_character_in_stream('AQIZQAZPN') ## output: A A A A A I I I I
