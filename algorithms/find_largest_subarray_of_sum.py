e12cb1dd1136991e2b1fd138eaf47ad96c7585d7
Sovan Panda <35234742+SovanCSE@users.noreply.github.com>
2023-10-13 12:47:38 +0530

    Update find_largest_subarray_of_sum.py

--- algorithms/find_largest_subarray_of_sum.py
+++ algorithms/find_largest_subarray_of_sum.py
@@ -8,21 +8,32 @@
 ## Example: LIST: [1, 2, 3, 7, 5] sum_values = 12 index_range = [2,4]
 ##################################################################################
 
-# arr = [1, -2, -2, 3, 1, 7, 5]
-arr = [1, 2, 3, 7, 5]
-k = 12
-i = j = array_max_length = sum_values = 0
-index_range = [-1]
-while j < len(arr):
-    sum_values += arr[j]
-    #While total sum value crossing the target sum
-    while sum_values > k:
-        sum_values -= arr[i]
-        i += 1
-    #if finds matching sum with largest subarray
-    if sum_values == k and (j-i) + 1 > array_max_length:
-        array_max_length = (j-i) + 1
-        index_range = [i+1, j+1]
-    #Move forward to next element if total sum less than or equal to target
-    j += 1
-print(f"MAX LENGTH: {array_max_length}, INDEX RANGE:  {index_range}")
+def largest_subarray_sum(nums, target):
+    
+    i = j = array_max_length = sum_values = 0
+    index_range = -1
+    
+    while j < len(nums):
+        
+        sum_values += nums[j]
+        
+        #While total sum value crossing the target sum
+        while sum_values > target:
+            sum_values -= nums[i]
+            i += 1
+            
+        #if finds matching sum with largest subarray
+        if sum_values == target and (j-i) + 1 > array_max_length:
+            array_max_length = (j-i) + 1
+            index_range = [i, j]
+            
+        #Move forward to next element if total sum less than or equal to target
+        j += 1
+
+    if index_range == -1:
+        return []
+    
+    return nums[index_range[0]:index_range[1]+1]
+
+largest_subarray_sum([1, 2, 3, 4, 5], 9) #[2, 3, 4]
+largest_subarray_sum([4, 3, 2, 1, 5], 5) #[3, 2]
